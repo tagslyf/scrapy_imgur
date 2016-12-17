d280ffe8b323d348705f880b3ba1572b52cfe71a
Ronald Tagolino <tags0516@gmail.com>
2016-12-17 08:01:43 +0000

    Added request to login and upload post. Use User-Agent to avoid captcha.

--- pyrequests.py
+++ pyrequests.py
@@ -1,55 +1,94 @@
-import socket
-import socks
+import json, uuid
 import requests
 from lxml import etree
 
-url = "http://www.imgur.com"
-print("Requesting {}".format(url))
+def main():
+	url = "http://www.imgur.com"
+	print("Requesting {}".format(url))
 
-s = requests.session()
+	s = requests.session()
 
-# login
-login_url = "https://imgur.com/signin?redirect=http%3A%2F%2Fimgur.com%2F"
-login_data = {'username': 'mmktest01', 'password': '123qwe!!'}
-headers = {'User-agent': 'Mozilla/5.0', 'referer': ''}
-html = s.post(login_url, login_data, headers = headers, timeout = 3)
-print(html, type(html))
-tree = etree.HTML(html.content)
-print(tree.xpath("//div[@class='dropdown-footer']"))
-print(tree.xpath("//div[@class='captcha']"), "Captcha")
-# html_login = open("html_login.txt", "wb")
-# html_login.write(html)
+	# login
+	login_url = "https://imgur.com/signin?redirect=http%3A%2F%2Fimgur.com%2F"
+	logout_url = ""
+	login_data = {
+		'username': 'mmkronald', 
+		'password': '123qwe!!'
+	}
+	headers = {
+		'User-agent': 'Mozilla/5.0', 
+		'referer': ''
+	}
+	html = s.post(login_url, login_data, headers = headers)
+	print("LOGOUT", html, type(html))
+	tree = etree.HTML(html.content)
+	print(tree.xpath("//div[@class='dropdown-footer']"), "Captcha", tree.xpath("//div[@class='captcha']"))
+	for a in tree.xpath("//div[@class='dropdown-footer']/a"):
+		print(a, a.text, "https:{}".format(a.xpath('@href')[0]))
+		if a.text == "logout":
+			logout_url = "https:{}".format(a.xpath('@href')[0])
 
-# upload URL http://imgur.com/upload
-post_url = "https://imgur.com/upload"
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36', 'referer': 'https://imgur.com/'}
-files = {'file': open('thumbnail00.png', 'rb')}
-print(26, files)
-html = s.post(post_url, files=files, headers=headers)
-print(html, html.text)
+	# upload URL http://imgur.com/upload; need to send request first in captcha URL https://imgur.com/upload/checkcaptcha
+	headers = {
+		'User-Agent': 'Mozilla/5.0', 
+		'Referer': 'https://imgur.com/',
+		'Host': 'imgur.com',
+		'Origin': 'https://imgur.com',
+		'Accept-Language': 'en-US,en;q=0.8'
+	}
 
-s.close()
+	upload_captcha_url = "https://imgur.com/upload/checkcaptcha"
+	data = {
+		'total_uploads': 1,
+		'create_album': 'true'
+	}
+	upload_captcha_html = s.post(upload_captcha_url, headers=headers, data=data)
+	upload_captcha_response = json.loads(upload_captcha_html.text)
+	print("UPLOAD CAPTCHA", upload_captcha_html, upload_captcha_response)
+	# print("UPLOAD CAPTCHA", upload_captcha_html, upload_captcha_response, upload_captcha_response['data']['new_album_id'])
 
+	post_url = "https://imgur.com/upload"
+	png_file = open('thumbnail00.png', 'rb')
+	
+	files = {
+		'Filedata': ('thumbnail00.png', png_file, 'image/png'),
+		'new_album_id': (None, upload_captcha_response['data']['new_album_id'])		
+	}
 
-# share URL http://imgur.com/ajax/share
+	post_html = s.post(post_url, files=files, headers=headers)
+	
+	# print("POST UPLOAD", post_html, post_html.headers['Content-Type'], "|||", post_html.request.headers['Content-Type'])
+	# print(post_html.text, type(post_html.text), "DELETEHASH: {}".format(json.loads(post_html.text)['data']['deletehash']))
 
-# url = "http://my-ip.herokuapp.com/"
-# s = requests.session()
+	post_html_response = json.loads(post_html.text)
+	print("POST UPLOAD", post_html, post_html_response)
 
-# ip = "50.30.164.238"
-# port = 45554
-# socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, ip, port)
-# socket.socket = socks.socksocket
+	# Update image title and description
+	update_url = "http://imgur.com/ajax/titledesc/{}".format(post_html_response['data']['deletehash'])
+	headers = {
+		'User-agent': 'Mozilla/5.0', 
+		'referer': 'http://imgur.com/{}'.format(post_html_response['data']['album'])
+	}
+	data = {
+		'title': str(uuid.uuid4()),
+		'description': "{} \n http://imgur.com/{}".format(str(uuid.uuid4()), post_html_response['data']['hash'])
+	}
+	print("UPDATE URL: {}".format(update_url))
+	print("UPDATE HEADERS: {}".format(headers))
+	print("UPDATE DATA: {}".format(data))
+	update_html = s.post(update_url, data=data, headers=headers)
+	print("UPDATE IMAGE TITLE/DESCRIPTION", update_html)
+	
+	# logout
+	logout_html = s.post(logout_url, headers = headers)
+	headers = {
+		'User-agent': 'Mozilla/5.0', 
+		'referer': ''
+	}
+	print("LOGOUT", logout_html)
 
-# html = s.get(url)
-# print(html.text)
+	s.cookies.clear()
+	s.close()
 
-# import socket
-# import socks
-# import requests
-# ip='localhost' # change your proxy's ip
-# port = 0000 # change your proxy's port
-# socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, ip, port)
-# socket.socket = socks.socksocket
-# url = u'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=inurl%E8%A2%8B'
-# print(requests.get(url).text)
+if __name__ == "__main__":
+	main()
